44b2423133aa24b2cc12f18fd6516d8d64d8ab24
Jozef <jozefw@sbcglobal.net>
2015-02-23 12:59:48 -0800

    part one

--- longestWord.js
+++ longestWord.js
@@ -1,6 +1,12 @@
 function LongestWord(sen) { 
-
-  // code goes here  
-  return sen; 
+var temp = [];
+var wSplit;
+wSplit = sen.split(" ");
+//   sorts largest word
+wSplit.sort(function(a,b){return b.length-a.length;});
+  console.log(wSplit);
+  
          
 }
+
+LongestWord("that is the world");
