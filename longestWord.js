9c6b60c199874ec0c920d0a23600f929f360a0fd
Jozef <jozefw@sbcglobal.net>
2015-02-23 14:04:51 -0800

    second variable

--- longestWord.js
+++ longestWord.js
@@ -1,12 +1,28 @@
 function LongestWord(sen) { 
-var temp = [];
-var wSplit;
-wSplit = sen.split(" ");
-//   sorts largest word
-wSplit.sort(function(a,b){return b.length-a.length;});
-  console.log(wSplit);
   
-         
+var temp = [],
+  wSplit = [],
+   twins = [],
+   bigWord;
+  
+wSplit = sen.split(" ");
+wSplit.sort(function(a,b){return a.length-b.length;});
+console.log(wSplit);
+  
+//   check for game length eq two
+  if ( wSplit.length === 2 ) {
+    console.log("end of game" + " " + wSplit[0]);
+    return wSplit[0];
+  }
+  
+  else {
+    for ( var i = 0; i< wSplit.length-1; i++ ) {
+       if ( wSplit[i].length === wSplit[i+1].length ) {
+         twins = wSplit[i];
+         console.log(twins + " win");
+       }
+    }
+  }       
 }
 
-LongestWord("that is the world");
+LongestWord("it is a");
