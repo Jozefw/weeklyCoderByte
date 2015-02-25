277083d2dc760590f46f152fc2dcb00290763948
Jozef <jozefw@sbcglobal.net>
2015-02-25 14:30:57 -0800

    add nums

---
+++ main.js
@@ -0,0 +1,10 @@
+function SimpleAdding(num) { 
+  var temp = 0;
+  for ( var i = num; i > 0; i-- )  {
+    temp = temp + (i);
+    console.log(temp);
+  } 
+console.log(temp);         
+}
+
+SimpleAdding(1);
