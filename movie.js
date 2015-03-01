a6c757b368f4e4b8fef4271c3e79379438cafce4
Jozef <jozefw@sbcglobal.net>
2015-02-28 20:01:00 -0800

    symbols

---
+++ movie.js
@@ -0,0 +1,11 @@
+$(function() {
+	console.log("loaded");
+
+	$('form').submit(function(e) {
+
+		$.ajax({
+			url:"http://www.omdbapi.com/?t=" + 
+		})
+	})
+
+});
--- simpleSymbols.js
+++ simpleSymbols.js
@@ -1,14 +1,17 @@
 function SimpleSymbols(str) { 
-
-  for ( var i = 0; i<str.length; i++ ) {
-     console.log(str.charCodeAt(i));
-    if ( str.charCodeAt(i)=== 43 && str.charCodeAt(i+2)===43 ) {
-      console.log("true");
+  for ( var j = 0; j<str.length; j++ ) {
+    if ( str[0] != "+" && str[str.length] != "+" ) {
+      console.log("not surrounded");
     }
-  }
-   
-   
-         
+    else {
+      for ( var i = 1; i<str.length-1; i++ ) {
+        if ( str[i-1] === "+" && str[i+1] === "+" ) {
+          console.log("ok");
+//           need a counter
+        }
+      }
+    }
+  }       
 }
 
-SimpleSymbols("+t+");
+SimpleSymbols("+t+rr+");
