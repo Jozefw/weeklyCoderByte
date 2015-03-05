2ef057bdecb1be84de999ddbf8c1bf81b66a770b
Jozef <jozefw@sbcglobal.net>
2015-03-05 15:19:31 -0800

    refactored

--- alphabetSoup.js
+++ alphabetSoup.js
@@ -1,11 +1,16 @@
 function AlphabetSoup(str) { 
-  var temp = [];
-  var solution = [];
-  str = str.toLowerCase();
-  for ( var j = 0; j <str.length; j++ ) {
-    temp.push(str.charCodeAt(j));
-    temp = temp.sort(function(a,b){return a-b;});
-  }
+
+    var temp = [];
+    var solution = [];
+    str = str.toLowerCase();
+
+    function toAscii() {
+      for ( var j = 0; j <str.length; j++ ) {
+        temp.push(str.charCodeAt(j));
+        temp = temp.sort(function(a,b){return a-b;});
+      }
+    }
+
    for ( var i = 0; i<temp.length; i++ ) {
     solution.push(String.fromCharCode(temp[i]));  
    }
---
+++ alphabetSoupRefactor.js
@@ -0,0 +1,28 @@
+function AlphabetSoup(str) { 
+
+    var temp = [];
+    var solution = [];
+    str = str.toLowerCase();
+  console.log(str);
+
+    function toAscii(str) {
+      for ( var j = 0; j < str.length; j++ ) {
+        temp.push(str.charCodeAt(j));
+        temp = temp.sort(function(a,b){return a-b;});
+      }
+      console.log(temp);
+      return toLetter(temp);
+    }
+
+    function toLetter(ascii) {
+     for ( var i = 0; i < ascii.length; i++ ) {
+      solution.push( String.fromCharCode(ascii[i] )); 
+
+     }
+       console.log(solution + " solution");
+       return solution;
+    }
+  return toAscii(str);
+}
+
+alert(AlphabetSoup("geTTing"));
